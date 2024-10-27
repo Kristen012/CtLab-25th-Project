86d8158ca9528bd90205b9bd866c0560f61bab51
fang <kitty7471@gmail.com>
2024-10-28 01:43:03 +0800

    attention dequant revise 1

---
+++ dequan_attention_weight_correct/kernel_attn_proj.cpp
@@ -0,0 +1,173 @@
+// Includes
+#include <stdint.h>
+#include <hls_math.h>
+#include <hls_half.h>
+#include <ap_int.h>
+#include "hls_print.h"
+
+#define S_MAX 128
+#define EMB 768
+#define IO_MAX 1024
+#define X_SIZE 98304
+
+#define NUM_HEAD 12
+#define HEAD_DIM 64
+
+#define DATA_SIZE_ATTN_WEIGHT_TILED 49152
+#define DATA_SIZE_ATTN_BIAS_TILED 64
+#define DATA_SIZE_PROJ_WEIGHT 589824
+#define DATA_SIZE_PROJ_BIAS 768
+
+#define K_V_MAX 8192 // 128*64
+#define QK_RES_MAX 16384 // 128*128
+#define ONE_HEAD 192
+
+#define NF 768
+#define NX 768
+#define BLOCK_SIZE 128
+#define GROUP_SIZE 128
+#define DATA_SIZE_PROJ_WEIGHT 589824
+#define DATA_SIZE_PROJ_BIAS 768
+
+#define I_BOUND (NX / GROUP_SIZE)
+#define J_BOUND (GROUP_SIZE * NF / BLOCK_SIZE)
+#define ZREO_BUFFER_SIZE (NF / BLOCK_SIZE)
+#define DATA_SIZE_WEIGHT (NX * NF)
+#define I_INDEX (DATA_SIZE_WEIGHT / BLOCK_SIZE / (NX / GROUP_SIZE))
+
+typedef ap_uint<512> int512_t;
+
+static void reshape(float* in, float* out_buf, int size) {
+#pragma HLS inline off
+change_dim:
+    for (int i = 0; i < size; i++) {
+#pragma HLS PIPELINE II=1
+        out_buf[i] = in[i];
+    }
+}
+static void reshape_weight(int512_t* qweight, int512_t* qzeros, float* scale, float* out_buf, int size) {
+#pragma HLS inline off
+change_dim:
+    ap_uint<512> zeros_buffer[ZREO_BUFFER_SIZE];
+    half scale_buffer[NF];
+    #pragma HLS bind_storage variable=zeros_buffer type=ram_t2p impl=bram
+    #pragma HLS bind_storage variable=scale_buffer type=ram_t2p impl=bram
+    #pragma HLS ARRAY_PARTITION variable=scale_buffer cyclic factor=4 dim=1
+    for (int i = 0; i < I_BOUND ; i++) {
+        #pragma HLS dataflow
+        for (int j = 0; j < ZREO_BUFFER_SIZE ; j++){
+            #pragma HLS PIPELINE II = 1
+            zeros_buffer[j] = qzeros[i*ZREO_BUFFER_SIZE + j];
+        }
+        for (int j = 0; j < NF; j++){
+            #pragma HLS PIPELINE II = 1
+            scale_buffer[j] = static_cast<half>(scale[i*NF + j]);
+        }
+        for(int j = 0; j < J_BOUND ; j++){
+            ap_uint<512> weight = qweight[i*I_INDEX + j]; // i(group index): every 128*2304 element plus 1 = read 2304 times plus 1; j(index of loop in a group): read 1 times plus 1
+            ap_uint<512> zero = zeros_buffer[(j * BLOCK_SIZE % NF) / BLOCK_SIZE];
+            for (int k = 0; k < BLOCK_SIZE ;k++){
+                #pragma HLS UNROLL factor=4 skip_exit_check
+                #pragma HLS PIPELINE II = 3
+                ap_uint<4> int4_weight = weight.range((k + 1) * 4 - 1, k * 4);
+                ap_uint<4> int4_zero = zero.range((k + 1) * 4 - 1, k * 4) + 1;
+                float dequant_weight = (static_cast<float>((int4_weight - int4_zero) * (scale_buffer[(j * BLOCK_SIZE + k) % NF])));
+                out_buf[(i * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k)] = dequant_weight;
+                hls::print("==============");
+                hls::print("weight index: %d", (i * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k));
+                hls::print("dequant_weight: %f", dequant_weight);
+            }
+        }
+    }
+}
+
+static void compute_attn(float* qkv_res_buf,
+                        float* c_proj_weight_buf,
+                        float* c_proj_bias_buf,
+                        int s,
+                        float* out_buf) {
+
+#pragma HLS DATAFLOW
+
+init_c_proj:
+    for (int i = 0; i<s; i++) {
+        for (int j = 0; j<EMB; j++) {
+#pragma HLS PIPELINE II=1
+#pragma HLS UNROLL skip_exit_check factor=8
+#pragma HLS loop_flatten
+            out_buf[i*EMB + j] = c_proj_bias_buf[j];
+        }
+    }
+    
+compute_c_proj:
+    for (int k = 0; k<NUM_HEAD; k++) {
+        for (int i = 0; i<s; i++) {
+            for (int m = 0; m<HEAD_DIM; m++) {
+
+                for (int j = 0; j<EMB; j++) {
+#pragma HLS PIPELINE II=1
+#pragma HLS UNROLL skip_exit_check factor=8
+#pragma HLS loop_flatten
+                int idx_o = i*EMB;
+                int qkv_idx = k*s*HEAD_DIM + i*HEAD_DIM + m;
+                float qkv_res_buf_tmp = qkv_res_buf[qkv_idx];
+                    int weight_idx = (k*HEAD_DIM + m)*EMB + j;
+                    out_buf[idx_o + j] += qkv_res_buf_tmp * c_proj_weight_buf[weight_idx];
+                }
+            }
+        }
+    }
+}
+
+
+static void store_result(float* buf, float* out, int size) {
+#pragma HLS inline off
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS PIPELINE II=1
+        out[i] = static_cast<half>(buf[i]);
+    }
+}
+
+extern "C" {
+
+void krnl_c_proj(float* qkv_res, int512_t* c_proj_qweight, int512_t*c_proj_zeros, float* c_proj_scale, float* c_proj_bias, int s, float* out, float* c_proj_weight_buf) {
+#pragma HLS INTERFACE m_axi port = qkv_res offset = slave bundle = gmem0 max_read_burst_length = 256
+#pragma HLS INTERFACE m_axi port = c_proj_qweight offset = slave bundle = gmem1 depth = DATA_SIZE_PROJ_WEIGHT/128 // inout
+#pragma HLS INTERFACE m_axi port = c_proj_zeros offset = slave bundle = gmem2 depth = DATA_SIZE_PROJ_WEIGHT/128/128 // inout
+#pragma HLS INTERFACE m_axi port = c_proj_scale offset = slave bundle = gmem3 depth = DATA_SIZE_PROJ_WEIGHT/128 // inout
+#pragma HLS INTERFACE m_axi port = c_proj_bias offset = slave bundle = gmem4 depth = DATA_SIZE_PROJ_BIAS // inout
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem5
+#pragma HLS INTERFACE m_axi port = c_proj_weight_buf offset = slave bundle = gmem6
+
+
+    float qkv_res_buf[X_SIZE];
+    // float c_proj_weight_buf[DATA_SIZE_PROJ_WEIGHT];
+    float c_proj_bias_buf[DATA_SIZE_PROJ_BIAS];
+    float out_buf[X_SIZE];
+
+#pragma HLS bind_storage variable=qkv_res_buf type=RAM_T2P impl=uram
+#pragma HLS bind_storage variable=c_proj_weight_buf type=RAM_T2P impl=uram
+#pragma HLS bind_storage variable=out_buf type=RAM_T2P impl=bram
+
+#pragma HLS ARRAY_RESHAPE variable=c_proj_weight_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=c_proj_bias_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=out_buf type=cyclic factor=8
+
+
+#pragma HLS DATAFLOW
+
+    reshape(qkv_res, qkv_res_buf, s*EMB);
+    reshape_weight(c_proj_qweight, c_proj_zeros, c_proj_scale, c_proj_weight_buf, DATA_SIZE_PROJ_WEIGHT);
+    
+    reshape(c_proj_bias, c_proj_bias_buf, DATA_SIZE_PROJ_BIAS);
+
+    compute_attn(qkv_res_buf, c_proj_weight_buf, c_proj_bias_buf, s, out_buf);
+
+    store_result(out_buf, out, s*EMB);
+    // for (int i = 0; i < DATA_SIZE_PROJ_WEIGHT; i++){
+    //     hls::print("index: %d",i);
+    //     hls::print("dequant_weight: %f", c_proj_weight_buf[i]);
+    // }
+}
+}
--- dequan_attention_weight_correct/kernel_attn_tiling.cpp
+++ dequan_attention_weight_correct/kernel_attn_tiling.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <hls_math.h>
 #include <hls_half.h>
+#include "hls_print.h"
 
 #define S_MAX 128
 #define EMB 768
@@ -21,12 +22,70 @@
 #define QK_RES_MAX 16384 // 128*128
 #define ONE_HEAD 192
 
+#define NF 64
+#define NX 768
+#define BLOCK_SIZE 128
+#define GROUP_SIZE 128
+
+#define I_BOUND (NX / GROUP_SIZE / 2)
+#define J_BOUND (GROUP_SIZE * NF / BLOCK_SIZE)
+#define DATA_SIZE_WEIGHT (NX * NF)
+#define I_INDEX (GROUP_SIZE * NF / BLOCK_SIZE)
+
+typedef ap_uint<512> intpacked_t;
+
 static void reshape(float* in, float* out_buf, int size) {
 #pragma HLS inline off
 change_dim:
     for (int i = 0; i < size; i++) {
 #pragma HLS PIPELINE II=1
         out_buf[i] = in[i];
+    }
+}
+static void reshape_weight(intpacked_t* qweight, intpacked_t* qzeros, float* scale, float* out_buf, int size) {
+#pragma HLS inline off
+#pragma HLS dataflow
+change_dim:
+    intpacked_t zeros_buffer;
+    half scale_buffer[128];
+    #pragma HLS bind_storage variable=scale_buffer type=ram_t2p impl=bram
+    #pragma HLS ARRAY_PARTITION variable=scale_buffer cyclic factor=4 dim=1
+    for (int i = 0; i < I_BOUND ; i++) {
+        zeros_buffer = qzeros[i];
+        for (int j = 0; j < 128; j++){
+            #pragma HLS PIPELINE II = 1
+            scale_buffer[j] = static_cast<half>(scale[i*2*NF + j]);
+        }
+        for(int j = 0; j < J_BOUND ; j++){
+            ap_uint<512> weight = qweight[(2*i)*I_INDEX + j]; // i(group index): every 128*2304 element plus 1 = read 2304 times plus 1; j(index of loop in a group): read 1 times plus 1
+            ap_uint<256> zeros = zeros_buffer.range(255,0);
+            for (int k = 0; k < BLOCK_SIZE ;k++){
+                #pragma HLS UNROLL factor=4 skip_exit_check
+                #pragma HLS PIPELINE II = 3
+                ap_uint<4> int4_weight = weight.range((k + 1) * 4 - 1, k * 4);
+                ap_uint<4> int4_zero = zeros.range(((k % 64) + 1) * 4 - 1, (k % 64) * 4) + 1;
+                float dequant_weight = (static_cast<float>((int4_weight - int4_zero) * (scale_buffer[k % 64])));
+                out_buf[((2*i) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k)] = dequant_weight;
+                hls::print("==============");
+                hls::print("weight index: %d", ((2*i) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k));
+                hls::print("dequant_weight: %f", out_buf[((2*i) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k)]);
+            }
+        }
+        for(int j = 0; j < J_BOUND ; j++){
+            ap_uint<512> weight = qweight[(2*i+1)*I_INDEX + j]; // i(group index): every 128*2304 element plus 1 = read 2304 times plus 1; j(index of loop in a group): read 1 times plus 1
+            ap_uint<256> zeros = zeros_buffer.range(511,256);
+            for (int k = 0; k < BLOCK_SIZE ;k++){
+                #pragma HLS UNROLL factor=4 skip_exit_check
+                #pragma HLS PIPELINE II = 3
+                ap_uint<4> int4_weight = weight.range((k + 1) * 4 - 1, k * 4);
+                ap_uint<4> int4_zero = zeros.range(((k % 64) + 1) * 4 - 1, (k % 64) * 4) + 1;
+                float dequant_weight = (static_cast<float>((int4_weight - int4_zero) * (scale_buffer[64 + (k % 64)])));
+                out_buf[((2*i+1) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k)] = dequant_weight;
+                hls::print("==============");
+                hls::print("weight index: %d", ((2*i+1) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k));
+                hls::print("dequant_weight: %f", out_buf[((2*i+1) * (GROUP_SIZE * NF) + j * BLOCK_SIZE + k)]);
+            }
+        }
     }
 }
 static void compute_attn(float* x_buf,
@@ -204,29 +263,42 @@ mem_wr:
 
 extern "C" {
 
-void krnl_attn(float* x, float* w_tiling_q, float* w_tiling_k, float* w_tiling_v,
+void krnl_attn(float* x,
+                intpacked_t* w_tiling_q_qwieght, intpacked_t*w_tiling_q_zeros, float* w_tiling_q_scale,
+                intpacked_t* w_tiling_k_qwieght, intpacked_t*w_tiling_k_zeros, float* w_tiling_k_scale,
+                intpacked_t* w_tiling_v_qwieght, intpacked_t*w_tiling_v_zeros, float* w_tiling_v_scale,
                 float* bias_tiling_q, float* bias_tiling_k, float* bias_tiling_v,
                 float* k_tiling, float* v_tiling,
-                int s, int iter, int host_wi, float* out, float* cur_k, float* cur_v) {
+                int s, int iter, int host_wi, float* out, float* cur_k, float* cur_v,
+                float* w_tiling_q_buf, float* w_tiling_k_buf, float* w_tiling_v_buf) {
 #pragma HLS INTERFACE m_axi port = x offset = slave bundle = gmem0 max_read_burst_length = 256
-#pragma HLS INTERFACE m_axi port = w_tiling_q offset = slave bundle = gmem1 depth = DATA_SIZE_ATTN_WEIGHT_TILED
-#pragma HLS INTERFACE m_axi port = w_tiling_k offset = slave bundle = gmem2 depth = DATA_SIZE_ATTN_WEIGHT_TILED
-#pragma HLS INTERFACE m_axi port = w_tiling_v offset = slave bundle = gmem3 depth = DATA_SIZE_ATTN_WEIGHT_TILED
-#pragma HLS INTERFACE m_axi port = bias_tiling_q offset = slave bundle = gmem4 depth = DATA_SIZE_ATTN_BIAS_TILED
-#pragma HLS INTERFACE m_axi port = bias_tiling_k offset = slave bundle = gmem4 depth = DATA_SIZE_ATTN_BIAS_TILED
-#pragma HLS INTERFACE m_axi port = bias_tiling_v offset = slave bundle = gmem4 depth = DATA_SIZE_ATTN_BIAS_TILED
-#pragma HLS INTERFACE m_axi port = k_tiling offset = slave bundle = gmem5 depth = K_V_MAX // inout
-#pragma HLS INTERFACE m_axi port = v_tiling offset = slave bundle = gmem6 depth = K_V_MAX // inout
-#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem9 // inout
+#pragma HLS INTERFACE m_axi port = w_tiling_q_qwieght offset = slave bundle = gmem1 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64
+#pragma HLS INTERFACE m_axi port = w_tiling_q_zeros offset = slave bundle = gmem2 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64/128
+#pragma HLS INTERFACE m_axi port = w_tiling_q_scale offset = slave bundle = gmem3 depth = DATA_SIZE_ATTN_WEIGHT_TILED/128
+#pragma HLS INTERFACE m_axi port = w_tiling_k_qwieght offset = slave bundle = gmem4 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64
+#pragma HLS INTERFACE m_axi port = w_tiling_k_zeros offset = slave bundle = gmem5 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64/128
+#pragma HLS INTERFACE m_axi port = w_tiling_k_scale offset = slave bundle = gmem6 depth = DATA_SIZE_ATTN_WEIGHT_TILED/128
+#pragma HLS INTERFACE m_axi port = w_tiling_v_qwieght offset = slave bundle = gmem7 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64
+#pragma HLS INTERFACE m_axi port = w_tiling_v_zeros offset = slave bundle = gmem8 depth = DATA_SIZE_ATTN_WEIGHT_TILED/64/128
+#pragma HLS INTERFACE m_axi port = w_tiling_v_scale offset = slave bundle = gmem9 depth = DATA_SIZE_ATTN_WEIGHT_TILED/128
+#pragma HLS INTERFACE m_axi port = bias_tiling_q offset = slave bundle = gmem10 depth = DATA_SIZE_ATTN_BIAS_TILED
+#pragma HLS INTERFACE m_axi port = bias_tiling_k offset = slave bundle = gmem11 depth = DATA_SIZE_ATTN_BIAS_TILED
+#pragma HLS INTERFACE m_axi port = bias_tiling_v offset = slave bundle = gmem12 depth = DATA_SIZE_ATTN_BIAS_TILED
+#pragma HLS INTERFACE m_axi port = k_tiling offset = slave bundle = gmem13 depth = K_V_MAX // inout
+#pragma HLS INTERFACE m_axi port = v_tiling offset = slave bundle = gmem14 depth = K_V_MAX // inout
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem15 // inout
+#pragma HLS INTERFACE m_axi port = w_tiling_q_buf offset = slave bundle = gmem16 // inout
+#pragma HLS INTERFACE m_axi port = w_tiling_k_buf offset = slave bundle = gmem17 // inout
+#pragma HLS INTERFACE m_axi port = w_tiling_v_buf offset = slave bundle = gmem18 // inout
 
 
     float x_buf[X_SIZE];
     float bias_tiling_q_buf[DATA_SIZE_ATTN_BIAS_TILED];
     float bias_tiling_k_buf[DATA_SIZE_ATTN_BIAS_TILED];
     float bias_tiling_v_buf[DATA_SIZE_ATTN_BIAS_TILED];
-    float w_tiling_q_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
-    float w_tiling_k_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
-    float w_tiling_v_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
+    // float w_tiling_q_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
+    // float w_tiling_k_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
+    // float w_tiling_v_buf[DATA_SIZE_ATTN_WEIGHT_TILED];
     float k_tiling_buf[MAX_CACHE_SIZE];
     float v_tiling_buf[MAX_CACHE_SIZE];
     float cur_k_buf[K_V_MAX];
@@ -243,26 +315,29 @@ void krnl_attn(float* x, float* w_tiling_q, float* w_tiling_k, float* w_tiling_v
 #pragma HLS bind_storage variable=cur_v_buf type=RAM_T2P impl=bram
 #pragma HLS bind_storage variable=out_buf type=RAM_T2P impl=bram
 
-// #pragma HLS ARRAY_RESHAPE variable=x_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=w_tiling_q_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=w_tiling_k_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=w_tiling_v_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=k_tiling_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=v_tiling_buf type=cyclic factor=8
-// #pragma HLS ARRAY_RESHAPE variable=out_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=x_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=w_tiling_q_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=w_tiling_k_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=w_tiling_v_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=k_tiling_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=v_tiling_buf type=cyclic factor=8
+#pragma HLS ARRAY_RESHAPE variable=out_buf type=cyclic factor=8
 
     int cache_out_size = (iter != 0) ? HEAD_DIM : s*HEAD_DIM;
     int query_s = (iter != 0) ? 1 : s;
     int out_size = query_s*EMB;
 // #pragma HLS DATAFLOW
-
     reshape(x, x_buf, query_s*EMB);
     reshape(bias_tiling_q, bias_tiling_q_buf, DATA_SIZE_ATTN_BIAS_TILED);
     reshape(bias_tiling_k, bias_tiling_k_buf, DATA_SIZE_ATTN_BIAS_TILED);
     reshape(bias_tiling_v, bias_tiling_v_buf, DATA_SIZE_ATTN_BIAS_TILED);
-    reshape(w_tiling_q, w_tiling_q_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
-    reshape(w_tiling_k, w_tiling_k_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
-    reshape(w_tiling_v, w_tiling_v_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
+    reshape_weight(w_tiling_q_qwieght, w_tiling_q_zeros, w_tiling_q_scale, w_tiling_q_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
+    reshape_weight(w_tiling_k_qwieght, w_tiling_k_zeros, w_tiling_k_scale, w_tiling_k_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
+    reshape_weight(w_tiling_v_qwieght, w_tiling_v_zeros, w_tiling_v_scale, w_tiling_v_buf, DATA_SIZE_ATTN_WEIGHT_TILED);
+    hls::print("start deq");
+    // reshape_weight(w_tiling_q_qwieght, w_tiling_q_zeros, w_tiling_q_scale, deq_q, DATA_SIZE_ATTN_WEIGHT_TILED);
+    // reshape_weight(w_tiling_k_qwieght, w_tiling_k_zeros, w_tiling_k_scale, deq_k, DATA_SIZE_ATTN_WEIGHT_TILED);
+    // reshape_weight(w_tiling_v_qwieght, w_tiling_v_zeros, w_tiling_v_scale, deq_v, DATA_SIZE_ATTN_WEIGHT_TILED);
     reshape(k_tiling, k_tiling_buf, s*HEAD_DIM);
     reshape(v_tiling, v_tiling_buf, s*HEAD_DIM);
 
@@ -274,6 +349,20 @@ void krnl_attn(float* x, float* w_tiling_q, float* w_tiling_k, float* w_tiling_v
     store_result(out_buf, out, out_size);
     store_result(cur_k_buf, cur_k, cache_out_size); // 存12次
     store_result(cur_v_buf, cur_v, cache_out_size); // 存12次
-
+    // hls::print("=======q_weight=======");
+    // for (int i = 0; i < DATA_SIZE_ATTN_WEIGHT_TILED; i++){
+    //     hls::print("index: %d",i);
+    //     hls::print("q_dequant_weight: %f", w_tiling_q_buf[i]);
+    // }
+    // hls::print("=======k_weight=======");
+    // for (int i = 0; i < DATA_SIZE_ATTN_WEIGHT_TILED; i++){
+    //     hls::print("index: %d",i);
+    //     hls::print("k_dequant_weight: %f", w_tiling_k_buf[i]);
+    // }
+    // hls::print("=======v_weight=======");
+    // for (int i = 0; i < DATA_SIZE_ATTN_WEIGHT_TILED; i++){
+    //     hls::print("index: %d",i);
+    //     hls::print("v_dequant_weight: %f", w_tiling_v_buf[i]);
+    // }
 }
 }
